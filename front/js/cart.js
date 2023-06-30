edc2238fada2b69826fb90dc326520a34d67b4ea
David Cotto <dacotto1984@gmail.com>
2023-06-30 11:00:30 -0400

    fixed bug where with 'editLocalStorage' variable.
    would produce null within the array.  Had to create else statement that returned lsItem.

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,17 +4,66 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (cartStorage !== null) {
         let cartItemsElement = document.querySelector('#cart__items');
-        const cartItems = JSON.parse(cartStorage);
 
-        cartItems.forEach((cartItem, index) => {
-            fetch(`http://localhost:3000/api/products/${cartItem.id}`)
-                .then(response => {
-                    if (response.ok) {
-                        return response.json();
+        cartInfo();
+
+        // Event delegation:
+        // it's a technique in JavaScript where you delegate the event handling of child elements to a parent element.
+        // It takes advantage of the "bubbling" behavior of events, where an event on a child element is propagated up (bubbled) to its ancestors.
+        // In this code, we're adding the event listener to 'cartItemsElement' which is the parent container for all cart items.
+        // When an 'input' event (i.e., a change in value) occurs on an input field inside any cart item, the event is bubbled up to 'cartItemsElement'.
+        // Then, in the event handler, we're checking if the event originated from an input field with the name 'itemQuantity'.
+        // This way, we only need to add one event listener on the parent, instead of adding individual listeners to each input field in each cart item.
+        // This is especially useful when the child elements (in this case, cart items) are dynamically added to the DOM, as it ensures the event handling works for future elements as well.
+        cartItemsElement.addEventListener('input', (event) => {
+            // Check if the event was triggered by an itemQuantity input
+            if (event.target.classList.contains('itemQuantity')) {
+                console.log(event.target.value);
+                const articleElement = event.target.closest('article');
+                const itemId = articleElement.dataset.id;
+                const localStorageData = JSON.parse(localStorage.getItem('cart'));
+                const editLocalStorage = localStorageData.map(lsItem => lsItem.id === itemId ? {...lsItem, quantity: event.target.value} : lsItem);
+
+                if (editLocalStorage && editLocalStorage.length > 0 ) {
+                    localStorage.setItem('cart', JSON.stringify(editLocalStorage));
+                    const getLocalStorage = JSON.parse(localStorage.getItem('cart'));
+                    const foundLsItem = getLocalStorage.find(lsItem => lsItem.id === itemId);
+                    if (foundLsItem) {
+                        const targetAncestorElement = event.target.closest('.cart__item__content');
+                        targetAncestorElement.querySelector('.cart__item__content__settings__quantity > p').innerText = `Quantity : ${foundLsItem.quantity}`;
                     }
-                })
-                .then(data => {
-                    const cartItemString = `
+                }
+            }
+
+            // Another Event Delegation using
+
+        });
+
+        cartItemsElement.addEventListener('click', (event) => {
+            if (event.target.classList.contains('deleteItem')) {
+                const idToRemove = event.target.closest('.cart__item').getAttribute('data-id');
+                const getLocalStorage = JSON.parse(localStorage.getItem('cart'));
+                const updatedCartList = getLocalStorage.filter(lsItem => lsItem.id !== idToRemove);
+                localStorage.setItem('cart', JSON.stringify(updatedCartList));
+                cartInfo();
+            }
+        });
+
+
+        function cartInfo() {
+            const cartItems = JSON.parse(cartStorage);
+
+            cartItems.forEach((cartItem, index) => {
+                fetch(`http://localhost:3000/api/products/${cartItem.id}`)
+                    .then(response => {
+                        if (response.ok) {
+                            return response.json();
+                        } else {
+                            throw new Error('Error fetching data');
+                        }
+                    })
+                    .then(data => {
+                        const cartItemString = `
                          <article class="cart__item" data-id="${cartItem.id}" data-color="${cartItem.color}">
                             <div class="cart__item__img">
                               <img src="${data.imageUrl}" alt="${data.altTxt}">
@@ -37,47 +86,16 @@ document.addEventListener('DOMContentLoaded', () => {
                             </div>
                           </article>
                     `;
-                    cartItemsElement.insertAdjacentHTML('beforeend', cartItemString);
+                        cartItemsElement.insertAdjacentHTML('beforeend', cartItemString);
 
-                })
-                .catch(err => {
-                    console.error(err);
-                });
+                    })
+                    .catch(err => {
+                        console.error(err);
+                    });
 
-        });
+            });
+        }
 
-        // Event delegation:
-        // it's a technique in JavaScript where you delegate the event handling of child elements to a parent element.
-        // It takes advantage of the "bubbling" behavior of events, where an event on a child element is propagated up (bubbled) to its ancestors.
-        // In this code, we're adding the event listener to 'cartItemsElement' which is the parent container for all cart items.
-        // When an 'input' event (i.e., a change in value) occurs on an input field inside any cart item, the event is bubbled up to 'cartItemsElement'.
-        // Then, in the event handler, we're checking if the event originated from an input field with the name 'itemQuantity'.
-        // This way, we only need to add one event listener on the parent, instead of adding individual listeners to each input field in each cart item.
-        // This is especially useful when the child elements (in this case, cart items) are dynamically added to the DOM, as it ensures the event handling works for future elements as well.
-        cartItemsElement.addEventListener('input', (event) => {
-            // Check if the event was triggered by an itemQuantity input
-            if (event.target.classList.contains('itemQuantity')) {
-                console.log(event.target.value);
-                const articleElement = event.target.closest('article');
-                const itemId = articleElement.dataset.id;
-                const localStorageData = JSON.parse(localStorage.getItem('cart'));
-                const editLocalStorage = localStorageData.map(lsItem => {
-                    if (lsItem.id === itemId) {
-                        return {...lsItem, quantity: event.target.value}
-                    }
-                });
-                if (editLocalStorage && editLocalStorage.length > 0 ) {
-                    localStorage.setItem('cart', JSON.stringify(editLocalStorage));
-                    const getLocalStorage = JSON.parse(localStorage.getItem('cart'));
-                    const foundLsItem = getLocalStorage.find(lsItem => lsItem.id === itemId);
-                    if (foundLsItem) {
-                        const targetAncestorElement = event.target.closest('.cart__item__content');
-                        targetAncestorElement.querySelector('.cart__item__content__settings__quantity > p').innerText = `Quantity : ${foundLsItem.quantity}`;
-                    }
-                }
-
-            }
-        });
 
     } else {
         console.error('Cart is empty');
