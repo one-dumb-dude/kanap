16cedcc4481b39e7c7b8cabbc2621b52b1d06c2b
David Cotto <dacotto1984@gmail.com>
2023-07-07 21:28:30 -0400

    added test plan file

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
     const params = new URLSearchParams(window.location.search);
-    const orderId = params.get('orderId');
-
-    document.querySelector('#orderId').innerText = orderId;
+    document.querySelector('#orderId').innerText = params.get('orderId');
 });
